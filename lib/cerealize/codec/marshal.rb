7f0acf2195c1454dec2540479a036784bda02ed8
Lin Jen-Shin <godfat@godfat.org>
2010-04-19 03:27:40 +0800

    TODO: extract base64 decorator, then we can use pure marshal

--- lib/cerealize/codec/marshal.rb
+++ lib/cerealize/codec/marshal.rb
@@ -5,6 +5,7 @@ module Cerealize
     module_function
 
     # Version 4.8 of Marshalize always base64s to "BA"
+    # TODO: extract base64 decorator, then we can use pure marshal
     def yours?(str)
       str[0..1] == 'BA'
     end
