267b9a79a92c5a285d8a4f9a9d532b0777cb0897
Lin Jen-Shin <godfat@godfat.org>
2011-06-28 23:00:30 +0800

    test/unit => bacon

--- test/test_all_codec.rb
+++ test/test_all_codec.rb
@@ -8,17 +8,14 @@ end
 
 require_relative 'common'
 
-class AllCodecTest < Test::Unit::TestCase
-  def setup
+describe Cerealize do
+  before do
     Boat.delete_all
-  end
-
-  def teardown
   end
 
   Cerealize.codec_names.each do |encoding|
 
-    define_method "test_#{encoding}_basic" do
+    should "#{encoding} basic" do
       set_encoding(encoding)
 
       b = Boat.create(:name => 'pequod')
@@ -27,39 +24,39 @@ class AllCodecTest < Test::Unit::TestCase
       b.save!
 
       b = Boat.find_by_name('pequod')
-      assert_equal Person, b.captain.class
-      assert_equal 'ahab', b.captain.name
+      b.captain.class.should.equal Person
+      b.captain.name .should.equal 'ahab'
 
       b.captain = Person.new('ishmael')             # make sure not frozen!
-      assert_equal 'ishmael', b.captain.name
+      b.captain.name.should.equal 'ishmael'
       b.captain.name = 'call me ishmael'
       b.save!
 
       b = Boat.find_by_name('pequod')
-      assert_equal 'call me ishmael', b.captain.name
+      b.captain.name.should.equal 'call me ishmael'
     end
 
     ['string', {:key1 => 'v', :key2 => [3,4]}].each do |captain|
-      define_method "test_#{encoding}_for_#{captain}_changed?" do
+      should "#{encoding} for #{captain}_changed?" do
         b = Boat.create(:captain => captain)
-        assert !b.changed?
+        b.changed?.should.equal false
         b.captain = b.captain
-        assert !b.changed?
+        b.changed?.should.equal false
         b.captain = captain
-        assert !b.changed?
+        b.changed?.should.equal false
         b.captain = captain.dup
-        assert !b.changed?
+        b.changed?.should.equal false
         b.captain = case captain
                       when String; captain.reverse
                       when Hash;   captain.merge(:key2 => [1,2])
                     end
-        assert b.changed?
+        b.changed?.should.equal true
       end
     end
 
     [ :none, :read, :read_write, :write ].each do |action|
 
-      define_method "test_#{encoding}_partial_update_#{action}" do
+      should "#{encoding} partial update #{action}" do
         set_encoding(encoding)
 
         Boat.delete_all
@@ -85,39 +82,38 @@ class AllCodecTest < Test::Unit::TestCase
 
         b3 = Boat.find_by_name 'titanic'
         if [:read_write, :write].include?(action)
-          assert_equal 'dawson', b3.captain.name
+          b3.captain.name.should.equal 'dawson'
         elsif action == :none
-          assert_equal 'rose', b3.captain.name
+          b3.captain.name.should.equal 'rose'
         elsif action == :read
-          assert %w{ rose dawson }.include?(b3.captain.name),
-            "captain.name is #{b3.captain.name}"
+          %w{ rose dawson }.should.include?(b3.captain.name)
         end
       end
     end
 
-    define_method "test_#{encoding}_just_save" do
+    should "#{encoding} just save" do
       set_encoding(encoding)
       b1 = Boat.new :name => 'alinghi'
       b1.save!
-      assert_equal nil, Boat.find_by_name('alinghi').captain
+      Boat.find_by_name('alinghi').captain.should.equal nil
     end
 
-    define_method "test_#{encoding}_just_save_writing" do
+    should "#{encoding} just save writing" do
       set_encoding(encoding)
       b1 = Boat.new :name => 'surprise'
       b1.captain = Person.new 'aubrey'
       b1.save!
-      assert_equal 'aubrey', Boat.find_by_name('surprise').captain.name
+      Boat.find_by_name('surprise').captain.name.should.equal 'aubrey'
     end
 
-    define_method "test_#{encoding}_just_reading" do
+    should "#{encoding} just reading" do
       set_encoding(encoding)
       b1 = Boat.create :name => 'black pearl', :captain => Person.new('sparrow')
       b2 = Boat.find_by_name 'black pearl'
-      assert_equal 'sparrow', b2.captain.name
+      b2.captain.name.should.equal 'sparrow'
     end
 
-    define_method "test_#{encoding}_deep_change" do
+    should "#{encoding} deep change" do
       set_encoding(encoding)
       b1 = Boat.create :name => 'bounty', :captain => (Person.new('bligh'))
 
@@ -125,10 +121,10 @@ class AllCodecTest < Test::Unit::TestCase
       b2.captain.hat = Hat.new('blue')
       b2.save!
 
-      assert_equal 'blue', Boat.find_by_name('bounty').captain.hat.color
+      Boat.find_by_name('bounty').captain.hat.color.should.equal 'blue'
     end
 
-    define_method "test_#{encoding}_deeper_change_object" do
+    should "#{encoding} deeper change object" do
       set_encoding(encoding)
       b1 = Boat.create :name => 'bounty', :captain => (Person.new('bligh'))
 
@@ -140,10 +136,10 @@ class AllCodecTest < Test::Unit::TestCase
       b2.captain.pocket << :francs
       b2.save!
 
-      assert_equal [ :pounds, :francs ], Boat.find_by_name('bounty').captain.pocket
+      Boat.find_by_name('bounty').captain.pocket.should.equal [:pounds, :francs]
     end
 
-    define_method "test_#{encoding}_deeper_change_hash" do
+    should "#{encoding} deeper change hash" do
       set_encoding(encoding)
       b1 = Boat.create :name => 'bounty', :captain => { :name => :bligh }
 
@@ -155,86 +151,86 @@ class AllCodecTest < Test::Unit::TestCase
       b2.captain[:pocket] << :francs
       b2.save!
 
-      assert_equal [ :pounds, :francs ], Boat.find_by_name('bounty').captain[:pocket]
+      Boat.find_by_name('bounty').captain[:pocket].should.equal [:pounds, :francs]
     end
 
 
-    define_method "test_#{encoding}_class_checking" do
+    should "#{encoding} class checking" do
       set_encoding(encoding)
       Boat.create :name => 'lollypop', :cargo => Blob.new
       Boat.find_by_name 'lollypop'
 
       Boat.create :name => 'minerva', :cargo => "WRONG KIND"
-      assert_raise ActiveRecord::SerializationTypeMismatch do
+      lambda{
         Boat.find_by_name('minerva').cargo
-      end
+      }.should.raise ActiveRecord::SerializationTypeMismatch
     end
 
-    define_method "test_#{encoding}_simple_hash" do
+    should "#{encoding} simple hash" do
       set_encoding(encoding)
       b = Boat.new
       h = { :name => 'skipper' }
       b.captain = h
-      assert_equal h, b.captain
+      b.captain.should.equal h
       b.save
-      assert_equal h, b.captain
+      b.captain.should.equal h
       b.captain[:actor] = 'alan hale'
-      assert_equal 'alan hale', b.captain[:actor]
+      b.captain[:actor].should.equal 'alan hale'
     end
 
-    define_method "test_#{encoding}_check_if_really_saved" do
+    should "#{encoding} check if really saved" do
       set_encoding(encoding)
       b = Boat.new
       b.captain = { :name => 'ramius' }
       b.save
 
       b2 = Boat.find(b.id)
-      assert_equal b.captain, b2.captain
+      b2.captain.should.equal b.captain
       b2.captain[:nationality] = 'russian'
       b2.save
 
       b3 = Boat.find(b2.id)
-      assert_equal 'russian', b3.captain[:nationality]
+      b3.captain[:nationality].should.equal 'russian'
     end
 
-    define_method "test_#{encoding}_array_cerealize" do
+    should "#{encoding} array cerealize" do
       set_encoding(encoding)
       b = Boat.new
       b.captain = [123, 456]
       b.save
-      assert_equal [123, 456], b.captain
-      assert_equal [123, 456], Boat.find(b.id).captain
+      b.captain.should.equal [123, 456]
+      Boat.find(b.id).captain.should.equal [123, 456]
     end
 
-    define_method "test_#{encoding}_repeated_saves" do
+    should "#{encoding} repeated saves" do
       set_encoding(encoding)
       b = Boat.new
       b.captain = { :name => 'kirk', :age => 23 }
       b.save
 
-      assert_equal 23, b.captain[:age]
-      assert_equal 23, Boat.first.captain[:age]
+               b.captain[:age].should.equal 23
+      Boat.first.captain[:age].should.equal 23
 
       b.captain[:age] += 1
       b.save
-      assert_equal 24, b.captain[:age]
-      assert_equal 24, Boat.first.captain[:age]
-
-      b.captain[:age] += 1
-      b.save
-      b.captain[:age] += 1
-      b.save
-      assert_equal 26, b.captain[:age]
-      assert_equal 26, Boat.first.captain[:age]
+               b.captain[:age].should.equal 24
+      Boat.first.captain[:age].should.equal 24
 
       b.captain[:age] += 1
       b.save
       b.captain[:age] += 1
       b.save
+               b.captain[:age].should.equal 26
+      Boat.first.captain[:age].should.equal 26
+
       b.captain[:age] += 1
       b.save
-      assert_equal 29, b.captain[:age]
-      assert_equal 29, Boat.first.captain[:age]
+      b.captain[:age] += 1
+      b.save
+      b.captain[:age] += 1
+      b.save
+               b.captain[:age].should.equal 29
+      Boat.first.captain[:age].should.equal 29
 
     end
   end
